78e3584269bf84b4efce2f90291439b17134c9ba
watcharaphol.d <dangpiem@hotmail.com>
2024-06-23 10:06:49 +0700

    Create logic to check my name

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
 // write code here
+let input = prompt("Guess my name:");
+input = input.toLowerCase();
+alert((input === "watcharaphol") ? `Correct! ${input} is my name` : `Wrong! That is not my name...`);
